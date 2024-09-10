d8a1382e47584eb0c098e8a0af77b0e1856d7812
Andrii Stetsenko <andruxa7@ukr.net>
2024-09-11 00:48:47 +0300

    Initial Commit

---
+++ LoopyBird/LoopyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopyBirdApp.swift
+//  LoopyBird
+//
+//  Created by Andrii Stetsenko on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoopyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
